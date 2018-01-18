9df64c5c83012293b52f3ebd050bee8865ee7c52
frishlin <frishlin@gmail.com>
2018-01-18 15:35:53 -0600

    add-function-validate //

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
 
   //Traer elementos del HTML
-  
-  
+
+
   var loadPage = function () {
-    
+
   };
 
   var paintContactsInHTML = function(contact) {
-   
+
     /* Crear elementos con DOM html al publicar contacto */
-     
-    
+
+
     //Asignando atributos y/o eventos
-    
+
 
     /* Asignando valores a los elementos*/
-    
+
 
     //Agregamos lo que creamos con el DOM a un elemento existente del HTML
-    
-  };
 
+  };
+//
   var addContact = function (e) {
     e.preventDefault();
     //Las siguientes lineas toman el valor que el usuario agrega en los inputs y los guardan en variables
-    
+
 
     //Con las variables separadas que obtuvimos creamos una estructura de datos unica, un objeto por cada contacto
-    
+
 
     /*Agregamos el contacto a nuestra data (arreglo declarado) para poderla filtrar y eliminar posteriormente*/
-    
+
     //La siguiente funcion se encarga de pintar los contactos en el html
-    
+
     /* Limpiando valores de formulario*/
-    
+
   };
 
-  
+
 
 
   var validateContact = function () {
